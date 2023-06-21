2ad8f3f0af901f673dfbdee1f8bd47d3a65b9058
IgboPharaoh <solomoneze62@gmail.com>
2023-06-21 11:01:20 +0100

    add sidebar to FederationAdmin

--- apps/guardian-ui/src/admin/FederationAdmin.tsx
+++ apps/guardian-ui/src/admin/FederationAdmin.tsx
@@ -16,6 +16,7 @@ import { CopyInput } from '@fedimint/ui';
 import { useAdminContext } from '../hooks';
 import { StatusResponse, Versions } from '../types';
 import { ReactComponent as CopyIcon } from '../assets/svgs/copy.svg';
+import { SideBar } from '../components/SideBar';
 
 export const FederationAdmin: React.FC = () => {
   const { api } = useAdminContext();
@@ -38,68 +39,71 @@ export const FederationAdmin: React.FC = () => {
     versions?.core.consensus !== undefined ? `${versions.core.consensus}` : '';
 
   return (
-    <Flex gap={4} flexDirection='column'>
-      <Flex gap={4}>
-        <Card flex='1'>
-          <CardHeader>Federation info</CardHeader>
-          <CardBody>
-            <Table>
-              <Tbody>
-                <Tr>
-                  <Td>Your status</Td>
-                  <Td>{status?.server}</Td>
-                </Tr>
-                <Tr>
-                  <Td>Epoch count</Td>
-                  <Td>{epochCount}</Td>
-                </Tr>
-                <Tr>
-                  <Td>API Version</Td>
-                  <Td>{apiVersion}</Td>
-                </Tr>
-                <Tr>
-                  <Td>Consensus version</Td>
-                  <Td>{consensusVersion}</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </CardBody>
-        </Card>
-        <Card flex='1'>
-          <CardHeader>Peer info</CardHeader>
-          <CardBody>
-            <Table>
-              <Thead>
-                <Tr>
-                  <Th>Name</Th>
-                  <Th>Status</Th>
-                </Tr>
-              </Thead>
-              {status && (
+    <Flex gap={4} flexDirection='row'>
+      <SideBar />
+      <Flex gap={4} flexDirection='column'>
+        <Flex gap={4}>
+          <Card flex='1'>
+            <CardHeader>Federation info</CardHeader>
+            <CardBody>
+              <Table>
                 <Tbody>
-                  {Object.entries(status?.consensus.status_by_peer).map(
-                    ([peerId, peerStatus]) => (
-                      <Tr key={peerId}>
-                        <Td>Peer {peerId}</Td>
-                        <Td>{peerStatus.connection_status}</Td>
-                      </Tr>
-                    )
-                  )}
+                  <Tr>
+                    <Td>Your status</Td>
+                    <Td>{status?.server}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Epoch count</Td>
+                    <Td>{epochCount}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>API Version</Td>
+                    <Td>{apiVersion}</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Consensus version</Td>
+                    <Td>{consensusVersion}</Td>
+                  </Tr>
                 </Tbody>
-              )}
-            </Table>
+              </Table>
+            </CardBody>
+          </Card>
+          <Card flex='1'>
+            <CardHeader>Peer info</CardHeader>
+            <CardBody>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th>Name</Th>
+                    <Th>Status</Th>
+                  </Tr>
+                </Thead>
+                {status && (
+                  <Tbody>
+                    {Object.entries(status?.consensus.status_by_peer).map(
+                      ([peerId, peerStatus]) => (
+                        <Tr key={peerId}>
+                          <Td>Peer {peerId}</Td>
+                          <Td>{peerStatus.connection_status}</Td>
+                        </Tr>
+                      )
+                    )}
+                  </Tbody>
+                )}
+              </Table>
+            </CardBody>
+          </Card>
+        </Flex>
+        <Card>
+          <CardHeader>Connection info</CardHeader>
+          <CardBody>
+            <CopyInput
+              value={connectionCode}
+              buttonLeftIcon={<Icon as={CopyIcon} />}
+            />
           </CardBody>
         </Card>
       </Flex>
-      <Card>
-        <CardHeader>Connection info</CardHeader>
-        <CardBody>
-          <CopyInput
-            value={connectionCode}
-            buttonLeftIcon={<Icon as={CopyIcon} />}
-          />
-        </CardBody>
-      </Card>
     </Flex>
   );
 };
