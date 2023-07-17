bc8e856b25744596daac1b0f62e0ac93760e8799
Justin Moon <mail@justinmoon.com>
2023-07-17 18:48:54 -0500

    fix: reduce the number of shell scripts

--- scripts/completion.sh
+++
@@ -1,4 +0,0 @@
-# shellcheck shell=bash
-
-eval "$(fedimint-cli completion bash)" || true
-eval "$(gateway-cli completion bash)" || true
--- scripts/mprocs-nix.sh
+++ scripts/mprocs-nix.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Run with 'source ./mprocs-nix.sh [guardian|gateway]"
-
-# If $TMP contains '/nix-shell.' it is already unique to the
-# nix shell instance, and appending more characters to it is
-# pointless. It only gets us closer to the 108 character limit
-# for named unix sockets (https://stackoverflow.com/a/34833072),
-# so let's not do it.
-
-if [[ "${TMP:-}" == *"/nix-shell."* ]]; then
-  FM_TEST_DIR="${2-$TMP}/fm-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 4 || true)"
-else
-  FM_TEST_DIR="${2-"$(mktemp --tmpdir -d XXXXX)"}"
+if [[ -z "${IN_NIX_SHELL:-}" ]]; then
+  echo "Run "nix develop" first"
+  exit 1
 fi
-export FM_TEST_DIR
+
+DEVIMINT_COMMAND=$1
+MPROCS_PATH=$2
+
+export FM_TEST_DIR="$TMP/fm-$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 4 || true)"
+export FM_FED_SIZE=4
 export FM_PID_FILE="$FM_TEST_DIR/.pid"
 export FM_LOGS_DIR="$FM_TEST_DIR/logs"
 
-echo "Setting up env variables in $FM_TEST_DIR"
+devimint dev-fed 2>/dev/null &
 
-mkdir -p "$FM_TEST_DIR"
-touch "$FM_PID_FILE"
+mkdir $FM_TEST_DIR
+mkdir $FM_LOGS_DIR
+touch $FM_PID_FILE
+echo $! >> $FM_PID_FILE
+eval "$(devimint $DEVIMINT_COMMAND)"
 
 # Function for killing processes stored in FM_PID_FILE in reverse-order they were created in
 function kill_fedimint_processes {
@@ -34,3 +33,6 @@ function kill_fedimint_processes {
 }
 
 trap kill_fedimint_processes EXIT
+
+echo "PATH: $MPROCS_PATH"
+mprocs -c $MPROCS_PATH
