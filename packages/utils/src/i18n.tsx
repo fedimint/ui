e336b87ea2d12fa61c993e5785fe6eb319b3d245
okjodom <okjodom@gmail.com>
2023-07-20 10:07:55 -0700

    feat: add typings to translation utils

--- packages/utils/src/i18n.tsx
+++ packages/utils/src/i18n.tsx
@@ -1,21 +1,24 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import { useTranslation } from 'react-i18next';
+export { useTranslation } from 'react-i18next';
 
-export const i18nProvider = (namespace: Array<any>) => {
-  const resources = namespace.reduce((acc, lng) => {
-    return {
-      ...acc,
-      [lng['key']]: { translation: lng['translation'] },
-    };
-  }, {});
+type Language = { key: string; description: string; translation: object };
+
+export const i18nProvider = (namespace: Language[]) => {
+  const resources = namespace.reduce(
+    (acc, { key, translation, description }) => {
+      return {
+        ...acc,
+        [key]: { translation, description },
+      };
+    },
+    {}
+  );
 
   i18n.use(LanguageDetector).use(initReactI18next).init({
     debug: true,
     resources,
     fallbackLng: 'en-US',
-  })
+  });
 };
-
-export { useTranslation };
