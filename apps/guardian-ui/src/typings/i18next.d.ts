085f6aa983bf43c652f7efaa5eecdb1255f1aa2c
Will O'Beirne <wbobeirne@gmail.com>
2023-07-22 12:33:19 -0500

    fix: remove leftover comments from type experimentation

--- apps/guardian-ui/src/typings/i18next.d.ts
+++ apps/guardian-ui/src/typings/i18next.d.ts
@@ -1,11 +1,9 @@
 import 'i18next';
 
-// import translation from './languages/en.json';
 import { languages } from './languages';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    // resources: { translation: typeof translation };
     resources: (typeof languages)[0];
     returnNull: false;
     returnObjects: false;
