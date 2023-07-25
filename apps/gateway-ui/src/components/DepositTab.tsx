d3d65d0c8360f9e7ff34767d844c5b805f13da57
IgboPharaoh <solomoneze62@gmail.com>
2023-07-25 21:46:13 +0100

    refactor: update design of DepositTab component

--- apps/gateway-ui/src/components/DepositTab.tsx
+++ apps/gateway-ui/src/components/DepositTab.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { TabPanel, Heading, Text, Flex, Box } from '@chakra-ui/react';
+import {
+  Text,
+  Flex,
+  useTheme,
+  Stack,
+  Link,
+  Box,
+  useClipboard,
+} from '@chakra-ui/react';
 import { ApiContext } from '../ApiProvider';
-import { TabHeader } from '.';
 import { QRCodeSVG } from 'qrcode.react';
 import { useTranslation } from '@fedimint/utils';
-
-export const DepositTabHeader = (): JSX.Element => {
-  const { t } = useTranslation();
-  return <TabHeader>{t('deposit-tab.tab-header')}</TabHeader>;
-};
+import { GatewayCard } from './GatewayCard';
+import { ReactComponent as CopyIcon } from '../assets/svgs/copy.svg';
+import { ReactComponent as LinkIcon } from '../assets/svgs/linkIcon.svg';
 
 export interface DepositTabProps {
   federationId: string;
-  active: boolean;
 }
 
 export const DepositTab = React.memo(function DepositTab({
@@ -22,6 +26,8 @@ export const DepositTab = React.memo(function DepositTab({
   const { gateway } = React.useContext(ApiContext);
   const [address, setAddress] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const theme = useTheme();
+  const { onCopy, hasCopied } = useClipboard(address);
 
   useEffect(() => {
     !address &&
@@ -38,33 +44,95 @@ export const DepositTab = React.memo(function DepositTab({
   }, [address, federationId, gateway]);
 
   return (
-    <TabPanel pl='8px' pr='8px'>
-      <Heading
-        fontWeight='500'
-        fontSize={{ base: '22', md: '24' }}
-        color='#1A202C'
-      >
-        {t('deposit-tab.header')}
-      </Heading>
-      <Flex
-        flexDir={{ base: 'column', md: 'row' }}
-        alignItems={{ base: 'left', md: 'center' }}
-        mb={4}
-      >
-        {address ? (
-          <>
-            <Text fontSize='lg' fontWeight='500' color='#1A202C' mr={2}>
-              {t('common.address')}:
-            </Text>
-            <Text fontSize='lg'>{address}</Text>
-          </>
-        ) : (
-          <Text>{error}</Text>
-        )}
-      </Flex>
-      <Box>
-        {address && <QRCodeSVG height='20em' width='20em' value={address} />}
-      </Box>
-    </TabPanel>
+    <GatewayCard>
+      <Stack spacing='12px'>
+        <Text
+          fontSize='lg'
+          fontWeight='600'
+          color={theme.colors.gray[900]}
+          fontFamily={theme.fonts.body}
+        >
+          {t('deposit-tab.tab_header')}
+        </Text>
+        <Text
+          fontSize='md'
+          color={theme.colors.gray[600]}
+          fontFamily={theme.fonts.body}
+        >
+          {t('deposit-tab.sentence-one')}
+        </Text>
+        <Stack spacing='24px'>
+          <Flex gap='4px' alignItems='flex-start'>
+            {address ? (
+              <Text
+                fontSize='md'
+                color={theme.colors.gray[900]}
+                fontFamily={theme.fonts.body}
+                overflowWrap='break-word'
+                wordBreak='break-word'
+                cursor='pointer'
+                onClick={onCopy}
+              >
+                {address}
+              </Text>
+            ) : (
+              <Text
+                fontSize='md'
+                color={theme.colors.red}
+                fontFamily={theme.fonts.body}
+              >
+                {error}
+              </Text>
+            )}
+            <Box>
+              {hasCopied ? (
+                <Text
+                  fontSize='xs'
+                  fontWeight='semibold'
+                  color={theme.colors.gray[900]}
+                  bgColor={theme.colors.gray[25]}
+                  fontFamily={theme.fonts.body}
+                  onClick={onCopy}
+                  p='4px 12px'
+                  borderRadius='16px'
+                  border={`1px solid ${theme.colors.gray[400]}`}
+                >
+                  {t('common.copied')}
+                </Text>
+              ) : (
+                <CopyIcon
+                  color={theme.colors.gray[900]}
+                  height='20px'
+                  width='20px'
+                  onClick={onCopy}
+                  cursor='pointer'
+                />
+              )}
+            </Box>
+          </Flex>
+          <Box>
+            {address && (
+              <QRCodeSVG height='200px' width='200px' value={address} />
+            )}
+          </Box>
+        </Stack>
+        <Flex gap='4px'>
+          <Link
+            fontSize='md'
+            color={theme.colors.blue[600]}
+            fontFamily={theme.fonts.body}
+            _hover={{ textDecoration: 'underline' }}
+            transition={`text-decoration 1s ease-in-out`}
+            href={`https://mempool.space/address/${address}`}
+            target='_blank'
+            rel='noreferrer'
+            w='fit-content'
+          >
+            {t('deposit-tab.mempool_deposit_link_text')}
+          </Link>
+          <LinkIcon color={theme.colors.blue[600]} />
+        </Flex>
+      </Stack>
+    </GatewayCard>
   );
 });
