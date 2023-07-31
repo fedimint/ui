1c16c6f1503b94ad3da1741858f0814fe1e9d57a
IgboPharaoh <solomoneze62@gmail.com>
2023-07-31 16:23:45 +0100

    feat: add progress indicator for fetching gatewayInfo

--- apps/gateway-ui/src/App.tsx
+++ apps/gateway-ui/src/App.tsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { Box, Heading, Stack, VStack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Stack,
+  VStack,
+  Text,
+  Flex,
+  useTheme,
+  CircularProgress,
+  CircularProgressLabel,
+} from '@chakra-ui/react';
 import { FederationCard, ConnectFederation } from './components';
 import { GatewayApi } from './GatewayApi';
 import { ApiProvider } from './ApiProvider';
 import { GatewayInfo, Federation } from './types';
 import { Wrapper } from '@fedimint/ui';
+import { useTranslation } from '@fedimint/utils';
 
 export const App = React.memo(function Admin(): JSX.Element {
   const gateway = useMemo(() => new GatewayApi(), []);
@@ -21,16 +32,22 @@ export const App = React.memo(function Admin(): JSX.Element {
   });
   const [showConnectFed, toggleShowConnectFed] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState(false);
+  const theme = useTheme();
+  const { t } = useTranslation();
 
   useEffect(() => {
+    setLoading(true);
     gateway
       .fetchInfo()
       .then((gatewayInfo: GatewayInfo) => {
         setGatewayInfo(gatewayInfo);
+        setLoading(false);
       })
       .catch(({ message, error }) => {
         console.error(error);
         setError(message);
+        setLoading(false);
       });
   }, [gateway]);
 
@@ -42,12 +59,42 @@ export const App = React.memo(function Admin(): JSX.Element {
     toggleShowConnectFed(!showConnectFed);
   };
 
+  if (loading) {
+    return (
+      <Flex
+        bgColor={theme.colors.white}
+        justifyContent='center'
+        alignItems='center'
+        h='100vh'
+      >
+        <CircularProgress
+          isIndeterminate={true}
+          color={theme.colors.blue[600]}
+          size='240px'
+          thickness='8px'
+          capIsRound={true}
+        >
+          <CircularProgressLabel
+            fontSize='md'
+            fontWeight='500'
+            color={theme.colors.gray[600]}
+            fontFamily={theme.fonts.body}
+            textAlign='center'
+            width='150px'
+          >
+            {t('admin.fetch-info-modal-text')}
+          </CircularProgressLabel>
+        </CircularProgress>
+      </Flex>
+    );
+  }
+
   return (
     <ApiProvider props={{ gateway }}>
       <Wrapper>
         {error ? (
           <VStack spacing={4}>
-            <Heading size='md'>Error</Heading>
+            <Heading size='md'>{t('common.error')}</Heading>
             <Text>{error}</Text>
           </VStack>
         ) : (
