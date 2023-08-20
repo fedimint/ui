3a79c7da8c1ac461570573ac57238a2dada4a6db
Ethan Tuttle <tuttle.ethan@protonmail.com>
2023-08-20 16:39:26 -0400

    feat: add Peer name to Peer Info table

--- apps/guardian-ui/src/admin/FederationAdmin.tsx
+++ apps/guardian-ui/src/admin/FederationAdmin.tsx
@@ -17,12 +17,47 @@ import {
 import { CopyInput } from '@fedimint/ui';
 import { useTranslation } from '@fedimint/utils';
 import { useAdminContext } from '../hooks';
-import { ConfigResponse, Gateway, StatusResponse, Versions } from '../types';
+import {
+  ClientConfig,
+  ConfigResponse,
+  FederationStatus,
+  Gateway,
+  PeerStatus,
+  StatusResponse,
+  Versions,
+} from '../types';
 import { AdminMain } from '../components/AdminMain';
 import { ConnectedNodes } from '../components/ConnectedNodes';
 import { LightningModuleRpc } from '../GuardianApi';
 import { ReactComponent as CopyIcon } from '../assets/svgs/copy.svg';
 import { Pill } from '../components/Pill';
+
+interface PeerData {
+  id: number;
+  name: string;
+  status: PeerStatus;
+}
+
+function generatePeerData(
+  config: ClientConfig,
+  federationStatus: FederationStatus
+): PeerData[] {
+  const peerDataArray: PeerData[] = [];
+  for (const [id, status] of Object.entries(federationStatus.status_by_peer)) {
+    const numericId = parseInt(id, 10);
+    if (Object.prototype.hasOwnProperty.call(config.api_endpoints, numericId)) {
+      const endpoint = config.api_endpoints[numericId];
+      if (endpoint) {
+        peerDataArray.push({
+          id: numericId,
+          name: endpoint.name,
+          status,
+        });
+      }
+    }
+  }
+  return peerDataArray;
+}
 
 export const FederationAdmin: React.FC = () => {
   const { api } = useAdminContext();
@@ -32,6 +67,7 @@ export const FederationAdmin: React.FC = () => {
   const [inviteCode, setInviteCode] = useState<string>('');
   const [config, setConfig] = useState<ConfigResponse>();
   const [gateways, setGateways] = useState<Gateway[]>([]);
+  const [peerData, setPeerData] = useState<PeerData[] | undefined>();
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -77,6 +113,13 @@ export const FederationAdmin: React.FC = () => {
     const guardiansStatusText = `${online} / ${totalPeers}`;
     return [guardiansStatusText, statusColor];
   }, [status]);
+
+  useEffect(() => {
+    if (status && status.federation && config) {
+      const data = generatePeerData(config.client_config, status.federation);
+      setPeerData(data);
+    }
+  }, [status, config]);
 
   const apiVersion = versions?.core.api.length
     ? `${versions.core.api[0].major}.${versions.core.api[0].minor}`
@@ -169,25 +212,21 @@ export const FederationAdmin: React.FC = () => {
               <Table>
                 <Thead>
                   <Tr>
+                    <Th>{t('federation-dashboard.peer-info.id-label')}</Th>
                     <Th>{t('federation-dashboard.peer-info.name-label')}</Th>
                     <Th>{t('federation-dashboard.peer-info.status-label')}</Th>
                   </Tr>
                 </Thead>
-                {status && (
-                  <Tbody>
-                    {Object.entries(
-                      status?.federation?.status_by_peer || {}
-                    ).map(([peerId, peerStatus]) => (
-                      <Tr key={peerId}>
-                        <Td>
-                          {t('federation-dashboard.peer-info.peer-id-prefix')}{' '}
-                          {peerId}
-                        </Td>
-                        <Td>{peerStatus.connection_status}</Td>
+                <Tbody>
+                  {peerData &&
+                    peerData?.map((x) => (
+                      <Tr key={x.id}>
+                        <Td>{x.id}</Td>
+                        <Td>{x.name}</Td>
+                        <Td>{x.status.connection_status}</Td>
                       </Tr>
                     ))}
-                  </Tbody>
-                )}
+                </Tbody>
               </Table>
             </CardBody>
           </Card>
