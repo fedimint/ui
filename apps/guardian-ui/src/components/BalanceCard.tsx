d7a07bd6f4bc2a8da0613ca73a3e8b1dfaeecacc
Will O'Beirne <wbobeirne@gmail.com>
2023-08-31 16:40:30 -0500

    feat(guardian-ui): hook up audit api to BalanceCard, adjust response types

--- apps/guardian-ui/src/components/BalanceCard.tsx
+++ apps/guardian-ui/src/components/BalanceCard.tsx
@@ -1,21 +1,37 @@
-import { Card, CardBody, CardHeader, Text } from '@chakra-ui/react';
+import { Card, CardBody, CardHeader, Skeleton, Text } from '@chakra-ui/react';
 import { KeyValues } from '@fedimint/ui';
-import { useTranslation } from '@fedimint/utils';
-import React, { useMemo } from 'react';
+import { MSats, formatMsatsToBtc, useTranslation } from '@fedimint/utils';
+import React, { useEffect, useMemo, useState } from 'react';
+import { AuditSummary } from '../types';
+import { useAdminContext } from '../hooks';
 
 export const BalanceCard: React.FC = () => {
   const { t } = useTranslation();
+  const { api } = useAdminContext();
+  const [auditSummary, setAuditSummary] = useState<AuditSummary>();
+
+  const walletBalance = auditSummary
+    ? auditSummary.module_summaries.wallet?.net_assets || (0 as MSats)
+    : undefined;
+
+  useEffect(() => {
+    api.audit().then(setAuditSummary).catch(console.error);
+  }, [api]);
 
   const keyValues = useMemo(
     () => [
       {
         key: 'bitcoin',
         label: t('common.bitcoin'),
-        // TODO: Use `audit` api to fill this in
-        value: '0.00000000',
+        value:
+          typeof walletBalance === 'number' ? (
+            formatMsatsToBtc(walletBalance)
+          ) : (
+            <Skeleton height='20px' />
+          ),
       },
     ],
-    [t]
+    [walletBalance, t]
   );
 
   return (
