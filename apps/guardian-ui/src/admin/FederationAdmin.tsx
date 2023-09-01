bd021679446d2f9480acbe9725c83b51b684b27b
Will O'Beirne <wbobeirne@gmail.com>
2023-09-01 13:45:30 -0500

    feat(guardian-ui): hook up wallet module config to bitcoin node card

--- apps/guardian-ui/src/admin/FederationAdmin.tsx
+++ apps/guardian-ui/src/admin/FederationAdmin.tsx
@@ -3,7 +3,11 @@ import { Flex, Box, Icon, Text, useTheme, Heading } from '@chakra-ui/react';
 import { CopyInput } from '@fedimint/ui';
 import { useTranslation } from '@fedimint/utils';
 import { useAdminContext } from '../hooks';
-import { ConfigResponse, StatusResponse } from '../types';
+import {
+  ConfigResponse,
+  ModulesConfigResponse,
+  StatusResponse,
+} from '../types';
 import { GatewaysCard } from '../components/GatewaysCard';
 import { ReactComponent as CopyIcon } from '../assets/svgs/copy.svg';
 import { GuardiansCard } from '../components/GuardiansCard';
@@ -17,12 +21,14 @@ export const FederationAdmin: React.FC = () => {
   const [status, setStatus] = useState<StatusResponse>();
   const [inviteCode, setInviteCode] = useState<string>('');
   const [config, setConfig] = useState<ConfigResponse>();
+  const [modulesConfigs, setModulesConfigs] = useState<ModulesConfigResponse>();
   const { t } = useTranslation();
 
   useEffect(() => {
     // TODO: poll server status
     api.status().then(setStatus).catch(console.error);
     api.inviteCode().then(setInviteCode).catch(console.error);
+    api.modulesConfig().then(setModulesConfigs).catch(console.error);
   }, [api]);
 
   useEffect(() => {
@@ -69,7 +75,7 @@ export const FederationAdmin: React.FC = () => {
           <FederationInfoCard status={status} />
           <Flex w='100%' direction='column' gap={5}>
             <BalanceCard />
-            <BitcoinNodeCard config={config} />
+            <BitcoinNodeCard modulesConfigs={modulesConfigs} />
           </Flex>
         </Flex>
         <GuardiansCard status={status} config={config} />
