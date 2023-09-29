8b437e540b65167dc61540d4dd0f64c7e30f6e9b
Harsh Goyal <goyalh862@gmail.com>
2023-09-29 18:50:41 +0530

    fix bug -  DKG status error message renders

--- apps/guardian-ui/src/components/RunDKG.tsx
+++ apps/guardian-ui/src/components/RunDKG.tsx
@@ -56,7 +56,7 @@ export const RunDKG: React.FC<Props> = ({ next }) => {
             setError(`${t('run-dkg.error-config')}`);
             break;
           default:
-            setError(`${t('run-dkg.error-default')} '${status}'`);
+            setError(`${t('run-dkg.error-default')} '${JSON.stringify(status)}'`);
         }
       } catch (err) {
         setError(formatApiErrorMessage(err));
