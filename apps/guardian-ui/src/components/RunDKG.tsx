9ab113868b9200f6869e8161a3452f4af153a0f7
Harsh Goyal <goyalh862@gmail.com>
2023-09-29 20:43:59 +0530

    adding status.server

--- apps/guardian-ui/src/components/RunDKG.tsx
+++ apps/guardian-ui/src/components/RunDKG.tsx
@@ -56,7 +56,7 @@ export const RunDKG: React.FC<Props> = ({ next }) => {
             setError(`${t('run-dkg.error-config')}`);
             break;
           default:
-            setError(`${t('run-dkg.error-default')} '${JSON.stringify(status)}'`);
+            setError(`${t('run-dkg.error-default')} '${JSON.stringify(status.server)}'`);
         }
       } catch (err) {
         setError(formatApiErrorMessage(err));
