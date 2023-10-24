1709b0b5d0d8ef38a138ccb4d8ccb100882bb9e5
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-10-24 13:16:25 +0530

    refactor: ban HStack and VStack components

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -20,6 +20,19 @@ module.exports = {
       { avoidEscape: true, allowTemplateLiterals: true },
     ],
     semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+    'no-restricted-imports': [
+      'error',
+      {
+        name: 'HStack',
+        message:
+          "Please use <Flex direction='row'> instead as it does not add any additional styling, where as HStack adds 0.5 rem margin to it's children.",
+      },
+      {
+        name: 'VStack',
+        message:
+          "Please use <Flex direction='column'> instead as it does not add any additional styling, where as VStack adds 0.5 rem margin to it's children.",
+      },
+    ],
   },
   settings: {
     react: {
