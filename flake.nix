5d0a492e36e57d203aafdd17ba6d542845561bd6
kody.low <kodylow7@gmail.com>
2023-11-07 14:55:41 -0800

    feat: add starship in nix develop devShell

--- flake.nix
+++ flake.nix
@@ -21,12 +21,14 @@
               pkgs.mprocs
               pkgs.nodejs
               pkgs.yarn
+              pkgs.starship
               fedimint.packages.${system}.devimint
               fedimint.packages.${system}.gateway-pkgs
               fedimint.packages.${system}.fedimint-pkgs
             ] ++ prev.nativeBuildInputs;
             shellHook = ''
               yarn install
+              eval "$(starship init bash)"
             '';
           });
         };
