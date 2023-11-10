1f59cd6ba32ef0bb803b2f9d56a1c0e991e3c7c2
Will O'Beirne <wbobeirne@gmail.com>
2023-11-10 17:14:57 -0600

    fix: re-add CardBody

--- apps/guardian-ui/src/components/GatewaysCard.tsx
+++ apps/guardian-ui/src/components/GatewaysCard.tsx
@@ -6,6 +6,7 @@ import {
   AlertTitle,
   Box,
   Card,
+  CardBody,
   CardHeader,
   Flex,
   Link,
@@ -111,25 +112,27 @@ export const GatewaysCard: React.FC<GatewaysCardProps> = ({ config }) => {
           {t('federation-dashboard.gateways.label')}
         </Text>
       </CardHeader>
-      {gateways.length === 0 ? (
-        <Flex justifyContent='center' mx={4}>
-          <Alert status='info'>
-            <AlertIcon as={InfoIcon} />
-            <Box>
-              <AlertTitle>
-                {t('federation-dashboard.gateways.no-gateways-info-title')}
-              </AlertTitle>
-              <AlertDescription>
-                {t(
-                  'federation-dashboard.gateways.no-gateways-info-description'
-                )}
-              </AlertDescription>
-            </Box>
-          </Alert>
-        </Flex>
-      ) : (
-        <Table columns={columns} rows={rows} />
-      )}
+      <CardBody>
+        {gateways.length === 0 ? (
+          <Flex justifyContent='center'>
+            <Alert status='info'>
+              <AlertIcon as={InfoIcon} />
+              <Box>
+                <AlertTitle>
+                  {t('federation-dashboard.gateways.no-gateways-info-title')}
+                </AlertTitle>
+                <AlertDescription>
+                  {t(
+                    'federation-dashboard.gateways.no-gateways-info-description'
+                  )}
+                </AlertDescription>
+              </Box>
+            </Alert>
+          </Flex>
+        ) : (
+          <Table columns={columns} rows={rows} />
+        )}
+      </CardBody>
     </Card>
   );
 };
