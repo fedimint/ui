084837fd741d411a0f80f57fb3c025ba0bfc6b81
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-02-16 16:45:43 -0800

    chore: use fedimint's overlay

--- flake.nix
+++ flake.nix
@@ -1,18 +1,23 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     fedimint = {
+      # temporarily switched to https://github.com/fedimint/fedimint/pull/4349
       url =
-        "github:fedimint/fedimint?rev=9ec5418ee002134188d4584cc645fe1b5ffba4f1";
+        "github:dpc/fedimint?rev=678d456a1e242fb952b92514f0e6a83083a9704d";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, fedimint }:
+  outputs = { self, flake-utils, fedimint }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        nixpkgs = fedimint.inputs.nixpkgs;
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = fedimint.overlays.fedimint;
+        };
         fmLib = fedimint.lib.${system};
-      in {
+      in
+      {
         devShells = fmLib.devShells // {
           default = fmLib.devShells.default.overrideAttrs (prev: {
             nativeBuildInputs = [
