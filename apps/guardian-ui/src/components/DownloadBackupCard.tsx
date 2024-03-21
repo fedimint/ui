ec32154cf2374abf794ac095af69579257bb34d9
okjodom <okjodom@gmail.com>
2024-03-21 21:51:17 +0300

    refactor: download backup card

--- apps/guardian-ui/src/components/DownloadBackupCard.tsx
+++ apps/guardian-ui/src/components/DownloadBackupCard.tsx
@@ -8,13 +8,16 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
-  Flex,
+  Card,
+  CardHeader,
+  Text,
+  CardBody,
 } from '@chakra-ui/react';
 import { useTranslation } from '@fedimint/utils';
 import { useAdminContext } from '../hooks';
 import { hexToBlob } from '../utils/api';
 
-export const DownloadBackup: React.FC = () => {
+export const DownloadBackupCard: React.FC = () => {
   const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
   const { api } = useAdminContext();
   const { t } = useTranslation();
@@ -34,11 +37,21 @@ export const DownloadBackup: React.FC = () => {
 
   return (
     <>
-      <Flex justifyContent='center'>
-        <Button colorScheme='blue' onClick={() => setIsWarningModalOpen(true)}>
-          {t('federation-dashboard.config.downloadBackup.button')}
-        </Button>
-      </Flex>
+      <Card flex='1'>
+        <CardHeader>
+          <Text size='lg' fontWeight='600'>
+            {t('federation-dashboard.config.backup.label')}
+          </Text>
+        </CardHeader>
+        <CardBody>
+          <Button
+            colorScheme='blue'
+            onClick={() => setIsWarningModalOpen(true)}
+          >
+            {t('federation-dashboard.config.backup.download-backup-button')}
+          </Button>
+        </CardBody>
+      </Card>
       <Modal
         isOpen={isWarningModalOpen}
         onClose={() => setIsWarningModalOpen(false)}
