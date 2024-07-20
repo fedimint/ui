3cfeaea77b21f15bd867715914fa9aba6d5fd486
kody.low <kodylow7@gmail.com>
2024-07-20 14:13:03 -0700

    chore: add config port check

--- scripts/mprocs-nix-guardian-manual.sh
+++ scripts/mprocs-nix-guardian-manual.sh
@@ -15,6 +15,10 @@ fi
 
 # Prompt user for config port
 read -p "Enter the config port for the fedimintd to point at: " CONFIG_PORT
+if ! [[ "$CONFIG_PORT" =~ ^[0-9]+$ ]] || [ "$CONFIG_PORT" -lt 1 ] || [ "$CONFIG_PORT" -gt 65535 ]; then
+  echo "Invalid port number."
+  exit 1
+fi
 
 # Export the environment variable
 export REACT_APP_FM_CONFIG_API="ws://127.0.0.1:$CONFIG_PORT"
