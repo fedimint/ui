ae6aca950155a146132bb3af698d4f93f7e7a231
kody.low <kodylow7@gmail.com>
2024-07-20 15:35:58 -0700

    feat: better deposit address stuff

--- apps/gateway-ui/src/components/index.tsx
+++ apps/gateway-ui/src/components/index.tsx
@@ -3,5 +3,5 @@ export { WithdrawCard } from './WithdrawCard';
 export { FederationCard } from './FederationCard';
 export { ConnectFederationModal } from './ConnectFederationModal';
 export { InfoCard } from './InfoCard';
-export { DepositCard } from './DepositCard';
+export { DepositCard } from './deposit/DepositCard';
 export { BalanceCard } from './BalanceCard';
