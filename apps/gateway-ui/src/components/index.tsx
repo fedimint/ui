d3faf3aab99a1f2fd1e598d75c53f2fb32c76791
kody.low <kodylow7@gmail.com>
2024-08-06 16:56:30 -0700

    fix: remove unused

--- apps/gateway-ui/src/components/index.tsx
+++ apps/gateway-ui/src/components/index.tsx
@@ -1,6 +1,4 @@
 export { GatewayCard } from './GatewayCard';
-export { WithdrawCard } from './WithdrawCard';
 export { ConnectFederationModal } from './ConnectFederationModal';
 export { ConnectFederation } from './federations/ConnectFederation';
 export { LightningCard } from './lightning/LightningCard';
-export { DepositCard } from './DepositCard';
