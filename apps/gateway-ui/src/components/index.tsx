1cef63141860e8778a04bfe83548c7d1bf2f9067
Tommy Volk <tvolk131@gmail.com>
2024-08-06 10:21:45 -0500

    chore: change ConnectFederation to ConnectFederationModal

--- apps/gateway-ui/src/components/index.tsx
+++ apps/gateway-ui/src/components/index.tsx
@@ -1,7 +1,7 @@
 export { GatewayCard } from './GatewayCard';
 export { WithdrawCard } from './WithdrawCard';
 export { FederationCard } from './FederationCard';
-export { ConnectFederation } from './ConnectFederation';
+export { ConnectFederationModal } from './ConnectFederationModal';
 export { InfoCard } from './InfoCard';
 export { DepositCard } from './DepositCard';
 export { BalanceCard } from './BalanceCard';
