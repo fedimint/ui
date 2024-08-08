fe65c7380632ca4c35881783d81b05752bf1bed9
kody.low <kodylow7@gmail.com>
2024-08-07 18:26:12 -0700

    fix: common wallet components + start on send screens

--- apps/gateway-ui/src/components/walletModal/receive/ReceiveOnchain.tsx
+++ apps/gateway-ui/src/components/walletModal/receive/ReceiveOnchain.tsx
@@ -1,25 +1,11 @@
 import React, { useCallback, useState } from 'react';
-import {
-  NumberInput,
-  NumberInputField,
-  Button,
-  Flex,
-  useClipboard,
-  Tabs,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
-  Box,
-} from '@chakra-ui/react';
+import { Flex, useClipboard } from '@chakra-ui/react';
 import { useTranslation } from '@fedimint/utils';
 import { Bip21Uri, FederationInfo, Sats } from '@fedimint/types';
 import { WalletModalState } from '../WalletModal';
 import FederationSelector from '../FederationSelector';
 import { ApiContext } from '../../../ApiProvider';
-import QRCode from 'qrcode.react';
-import { FiCopy } from 'react-icons/fi';
-import { AmountInput, CreateButton, QRCodeTabs } from '.';
+import { AmountInput, CreateButton, QRCodeTabs } from '..';
 
 interface ReceiveOnchainProps {
   federations: FederationInfo[];
@@ -82,101 +68,6 @@ const ReceiveOnchain: React.FC<ReceiveOnchainProps> = ({
         onClick={handleCreateDepositAddress}
         label={t('wallet-modal.receive.create-deposit-address')}
       />
-    </Flex>
-  );
-};
-
-interface AddressInfoProps {
-  bip21Uri: Bip21Uri | undefined;
-  onCopyUri: () => void;
-  onCopyAddress: () => void;
-}
-
-const AddressInfo: React.FC<AddressInfoProps> = ({
-  bip21Uri,
-  onCopyUri,
-  onCopyAddress,
-}) => {
-  const { t } = useTranslation();
-
-  return (
-    <Flex
-      direction='column'
-      alignItems='center'
-      gap={4}
-      maxWidth='300px'
-      mx='auto'
-    >
-      <Tabs width='100%' isFitted>
-        <TabList>
-          <Tab>{t('common.uri')}</Tab>
-          <Tab>{t('common.address')}</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Flex
-              direction='column'
-              alignItems='center'
-              justifyContent='center'
-              width='100%'
-            >
-              <QRCode value={bip21Uri?.toString() ?? ''} size={200} />
-              <Flex direction='column' gap={2} width='100%' mt={4}>
-                <Button
-                  onClick={onCopyUri}
-                  leftIcon={<FiCopy />}
-                  width='100%'
-                  variant='outline'
-                >
-                  {t('common.copy')}
-                </Button>
-                <Box
-                  borderWidth={1}
-                  borderRadius='md'
-                  p={2}
-                  bg='gray.50'
-                  color='gray.600'
-                  fontSize='sm'
-                  wordBreak='break-all'
-                >
-                  {bip21Uri?.toString() ?? ''}
-                </Box>
-              </Flex>
-            </Flex>
-          </TabPanel>
-          <TabPanel>
-            <Flex
-              direction='column'
-              alignItems='center'
-              justifyContent='center'
-              width='100%'
-            >
-              <QRCode value={bip21Uri?.address ?? ''} size={200} />
-              <Flex direction='column' gap={2} width='100%' mt={4}>
-                <Button
-                  onClick={onCopyAddress}
-                  leftIcon={<FiCopy />}
-                  width='100%'
-                  variant='outline'
-                >
-                  {t('common.copy')}
-                </Button>
-                <Box
-                  borderWidth={1}
-                  borderRadius='md'
-                  p={2}
-                  bg='gray.50'
-                  color='gray.600'
-                  fontSize='sm'
-                  wordBreak='break-all'
-                >
-                  {bip21Uri?.address ?? ''}
-                </Box>
-              </Flex>
-            </Flex>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
     </Flex>
   );
 };
