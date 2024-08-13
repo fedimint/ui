ec10cd1bc787a94ec73d47664ce35765d54c426d
kody.low <kodylow7@gmail.com>
2024-08-12 17:00:10 -0700

    fix: ignore any for json with kind

--- packages/types/src/gateway.ts
+++ packages/types/src/gateway.ts
@@ -90,6 +90,7 @@ export interface GlobalClientConfig {
 
 export interface JsonWithKind {
   kind: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 }
 
