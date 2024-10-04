b403dba1fda9e84fe3002a3e3b63fbed23c4e85c
kody.low <kodylow7@gmail.com>
2024-10-03 19:52:07 -0700

    chore: type masterpassword hook

--- apps/router/src/hooks/useMasterPassword.tsx
+++ apps/router/src/hooks/useMasterPassword.tsx
@@ -1,8 +1,19 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
-export const useMasterPassword = () => {
-  const { masterPassword, setMasterPassword, isMasterPasswordSet } =
-    useContext(AuthContext);
+type MasterPasswordHook = {
+  masterPassword: string | null;
+  setMasterPassword: (password: string) => void;
+  isMasterPasswordSet: boolean;
+};
+
+export const useMasterPassword = (): MasterPasswordHook => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useMasterPassword must be used within an AuthProvider');
+  }
+
+  const { masterPassword, setMasterPassword, isMasterPasswordSet } = context;
   return { masterPassword, setMasterPassword, isMasterPasswordSet };
 };
