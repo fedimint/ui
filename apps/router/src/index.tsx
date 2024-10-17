c520cb6b7e05fe03c9156a7d272a6ddf47bfaa05
kody.low <kodylow7@gmail.com>
2024-10-17 14:05:54 -0700

    fix: fix

--- apps/router/src/index.tsx
+++ apps/router/src/index.tsx
@@ -15,15 +15,17 @@ import { ColorModeScript } from '@chakra-ui/react';
 
 import { i18nProvider, sha256Hash } from '@fedimint/utils';
 import { languages } from './languages';
-import { AppContextProvider } from './context/AppContext';
+import { APP_ACTION_TYPE, AppContextProvider } from './context/AppContext';
 import { HomePage } from './home/HomePage';
 import { GuardianContextProvider } from './context/guardian/GuardianContext';
 import { GatewayContextProvider } from './context/gateway/GatewayContext';
 import { Wrapper } from './components/Wrapper';
+import { useAppContext } from './context/hooks';
 
 i18nProvider(languages);
 
 const App = () => {
+  const { dispatch } = useAppContext();
   const [redirectPath, setRedirectPath] = useState<string | null>(null);
 
   useEffect(() => {
@@ -31,6 +33,18 @@ const App = () => {
       if (process.env.REACT_APP_FM_CONFIG_API) {
         try {
           const hash = await sha256Hash(process.env.REACT_APP_FM_CONFIG_API);
+          dispatch({
+            type: APP_ACTION_TYPE.ADD_GUARDIAN,
+            payload: {
+              id: hash,
+              guardian: {
+                config: {
+                  id: hash,
+                  baseUrl: process.env.REACT_APP_FM_CONFIG_API,
+                },
+              },
+            },
+          });
           setRedirectPath(`/guardian/${hash}`);
         } catch (e) {
           console.error(e);
@@ -38,6 +52,18 @@ const App = () => {
       } else if (process.env.REACT_APP_FM_GATEWAY_API) {
         try {
           const hash = await sha256Hash(process.env.REACT_APP_FM_GATEWAY_API);
+          dispatch({
+            type: APP_ACTION_TYPE.ADD_GATEWAY,
+            payload: {
+              id: hash,
+              gateway: {
+                config: {
+                  id: hash,
+                  baseUrl: process.env.REACT_APP_FM_GATEWAY_API,
+                },
+              },
+            },
+          });
           setRedirectPath(`/gateway/${hash}`);
         } catch (e) {
           console.error(e);
@@ -46,7 +72,7 @@ const App = () => {
     };
 
     calculateRedirectPath();
-  }, []);
+  }, [dispatch]);
 
   return (
     <Router>
