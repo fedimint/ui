dda934a73174982f00d3a1c6a595967dbebe6cf4
kody.low <kodylow7@gmail.com>
2024-10-21 14:52:47 -0700

    fix: fix

--- apps/router/src/guardian-ui/setupProxy.js
+++ apps/router/src/guardian-ui/setupProxy.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
   app.use('/config.json', (_req, res) => {
     res.json({
-      fm_config_api: process.env.REACT_APP_FM_CONFIG_API,
+      baseUrl: process.env.REACT_APP_FM_CONFIG_API,
       tos: process.env.REACT_APP_TOS,
     });
   });
